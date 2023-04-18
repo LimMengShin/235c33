8e11204569f73abcfdfb46fd92930a8f59455d68
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2023-04-18 15:59:26 +0800

    stuff

--- Class Site/app.py
+++ Class Site/app.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template, request
+from cs50 import SQL
 
 app = Flask(__name__)
+
+NAMES = ['Amelia', 'Gillian', 'Louissa', 'Yong Jia', 'Isis', 'Winona', 'Maydalynn', 'Min Jia', 'Nuo Xin', 'Yi Xin', 'Justin', 'Toby', 'Ethan', 'Zhong Yu', 'Kingster', 'Jun Rui', 'Xiang Ling', 'Hua Yu', 'Javier', 'Meng Shin', 'Matthew', 'Cayden', 'Reidon', 'Yun Hao', 'Nicholas', 'Theodore', 'Xander', 'Aaron']
+funds = {}
+
+db = SQL("sqlite:///class_funds.db")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html")
+
+
+@app.route("/reset", methods=["GET", "POST"])
+def reset():
+    for name in NAMES:
+        db.execute("INSERT INTO class_funds (name, funds) VALUES(?, ?)", name, 0)
+    funds = db.execute("SELECT * FROM class_funds")
+    return render_template("class_funds.html", funds=funds)
