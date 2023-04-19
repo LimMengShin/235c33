345d55f0544354883f85fdb1df1de7fe88ca98d4
LimMengShin <lim_meng_shin@s2019.ssts.edu.sg>
2023-04-19 22:07:12 +0800

    Add functionality to update funds + validation

--- Class Site/app.py
+++ Class Site/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 
+def amt_is_valid(amt):
+    try:
+        amt = float(amt)
+    except ValueError:
+        return False
+    else:
+        return True
+
 app = Flask(__name__)
 
 NAMES = ['Amelia', 'Gillian', 'Louissa', 'Yong Jia', 'Isis', 'Winona', 'Maydalynn', 'Min Jia', 'Nuo Xin', 'Yi Xin', 'Justin', 'Toby', 'Ethan', 'Zhong Yu', 'Kingster', 'Jun Rui', 'Xiang Ling', 'Hua Yu', 'Javier', 'Meng Shin', 'Matthew', 'Cayden', 'Reidon', 'Yun Hao', 'Nicholas', 'Theodore', 'Xander', 'Aaron']
-funds = {}
+GROUPS = ['Add', 'Subtract', 'H2 Physics', 'H2 Mathematics', 'H2 Economics', 'H2 Computing']
+d = {
+    'H2 Physics': 'h2_physics',
+    'H2 Mathematics': 'h2_math',
+    'H2 Economics': 'h2_econs',
+    'H2 Computing': 'h2_comp',
+}
+funds = []
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -28,18 +43,34 @@ def reset():
 def funds():
     with sqlite3.connect("class_funds.db") as con:
         cur = con.cursor()
-        funds = cur.execute("SELECT * from class_funds").fetchall()
-        con.commit()
-
-    if request.method == "GET":
-        return render_template("class_funds.html", funds=funds)
+        funds = [list(fund) for fund in cur.execute("SELECT * from class_funds").fetchall()]
             
-    elif request.method == "POST":
-        group = request.form.get("group")
-        amt = request.form.get("amt")
-        if not amt.isdigit():
-            return
-        elif not group:
-            return
+        if request.method == "GET":
+            return render_template("class_funds.html", funds=funds, groups=GROUPS)
+            
+        elif request.method == "POST":
+            group = request.form.get("group")
+            amt = request.form.get("amt")
+            if not group or group not in GROUPS or not amt or not amt_is_valid(amt):
+                return "failure"
+            else:
+                amt = float(amt)*100
+                if group == "Add":
+                    for fund in funds:
+                        fund[2] += int(amt)
+                elif group == "Subtract":
+                    for fund in funds:
+                        fund[2] -= int(amt)
+                else:
+                    subject = d[group]
+                    students = cur.execute(f"SELECT * from {subject}").fetchall()
+                    for fund in funds:
+                        if (fund[0], fund[1]) in students:
+                            fund[2] -= int(amt)
+                
+                for fund in funds:
+                    cur.execute("UPDATE class_funds SET funds = ? WHERE id = ?", (fund[2], fund[0]))
+            funds = [list(fund) for fund in cur.execute("SELECT * from class_funds").fetchall()]
 
-        return render_template("class_funds.html", funds=funds, group=group, amt=amt)
+            return render_template("class_funds.html", funds=funds, groups=GROUPS)
+    
